b9111d26c5b173482a063fbcdefe635be13d03c4
Nifemi <deyeminifemi@gmail.com>
2019-12-23 16:15:08 +0100

    Replicated design Added hiding or showing nav on scroll based on direction Added toggling fixed state of sidebar

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+let prevTop = 0;
+window.addEventListener('scroll',() => {
+    //Toggle sidebar fixed and absolute
+    const sidebar = document.querySelector('.sidebar');
+    if(window.scrollY >= window.innerHeight) {
+        sidebar.classList.add('sidebar--fixed');
+    }else{
+        sidebar.classList.remove('sidebar--fixed');
+    }
+
+    //Show navbar
+    const nav = document.querySelector('nav');
+    if(window.scrollY < prevTop){
+        nav.classList.add('is-showing');
+    }else{
+        nav.classList.remove('is-showing');
+    }
+    if(window.scrollY === 0){
+        nav.classList.add('hide-boxshadow')
+    }else{
+        nav.classList.remove('hide-boxshadow')
+    }
+    prevTop = window.scrollY;
+});
+
+const getDistanceFromTop = (element,current = 0) => element.parentElement ? getDistanceFromTop(element.parentElement,element.offsetTop + current) : current;
+
+const lastDropdown = document.querySelector('.dropdown:last-of-type .dropdown__button');
+const sideLine = document.querySelector('.sideline');
+
+sideLine.style.width = `${getDistanceFromTop(lastDropdown) + 45 - 110}px`;
+
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+   const button = dropdown.querySelector('.dropdown__button');
+   const content = document.querySelector('.dropdown__content');
+   button.addEventListener('click', () => {
+       if(content.classList.contains('is-active')){
+           content.classList.remove('is-active');
+       }else{
+           content.classList.add('is-active');
+       }
+   })
+});
